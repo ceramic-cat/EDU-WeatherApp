3213c193ab94881737eb5a31f6d621b4dcd56351
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2024-11-07 16:27:38 +0100

    Added several data fields and support for importing and displaying weather icon.

--- api.js
+++ api.js
@@ -1,8 +1,10 @@
 
 const apiKey = "be79937bd89750df6fe78cbc16cdd92d";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=${apiKey}&units=metric`;
 
-function getWeather() {
+function getWeather(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
   fetch(apiUrl)
     .then((response) => response.json())  
     .then((data) => {
@@ -10,11 +12,28 @@ function getWeather() {
       console.log(data);  // For debugging
       const weatherDescription = data.weather[0].description;
       const temp = data.main.temp;
+      const feels_like = data.main.feels_like;
       const humidity = data.main.humidity;
+      const pressure = data.main.pressure;
+      const wind = data.wind.speed;
+      const icon_id = data.weather[0].icon;
+      const iconUrl = `https://openweathermap.org/img/wn/${icon_id}@2x.png`
+
+      console.log(icon_id);
       document.getElementById("print-out").innerHTML = `
+        ${city} <br>
+
+        
+
         Weather: ${weatherDescription}<br>
+        <img src="${iconUrl}" alt="Weather icon" > <br>
+
+
         Temperature: ${temp}°C<br>
-        Humidity: ${humidity}%
+        Feels like: ${feels_like}°C<br>
+        Humidity: ${humidity}% <br>
+        Air pressure: ${pressure} kPa<br>
+        Wind: ${wind} m/s<br>
       `;
     })
     .catch((error) => {
@@ -23,5 +42,5 @@ function getWeather() {
     });
 }
 
-// Call the function to fetch weather data
-getWeather();
+
+
