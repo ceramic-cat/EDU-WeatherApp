11cfe8cf8e14d83470cb0bbb2456874899425ea9
ceramic-cat <120321392+ceramic-cat@users.noreply.github.com>
2024-11-10 17:27:48 +0100

    Reverted to previous code before merge. CatFact Api is down, changed it to show a useless fact instead. Updated text in the fact-notifier. Gave the notification a width, since the useless facts can be very long.

--- cat-facts.js
+++ cat-facts.js
@@ -1,45 +1,39 @@
-function getCatFact() {
-  const catApiUrl = "https://cat-fact.herokuapp.com/facts";
-  return fetch(catApiUrl)
+function getRandomFact() {
+  const factApiUrl = "https://uselessfacts.jsph.pl/api/v2/facts/random";
+  return fetch(factApiUrl)
     .then((data) => data.json())
     .then((data) => {
-      let fact = data[0].text;
-      console.log("Fetched cat fact:", fact);
+      let fact = data.text;
+      console.log("Fetched random fact:", fact);
       return fact;
     })
-    .catch((error) => 
-    {
-      console.error("Error catching cat facts: ", error);
-      return "Sorry, I could not catch a cat fact at this time";
-    })
-    
-    ;
+    .catch((error) => {
+      console.error("Unable to get a random fact: ", error);
+      return "No random facts available at this time! 😿";
+    });
 }
 
 // Show the catfact
-function showCatFact() {
+function showRandomFact() {
   const notification = document.getElementById("cat-fact-container");
   const paragraph = document.getElementById("cat-fact-paragraph");
 
   // Call getCatFact and update paragraph after the fact is fetched
-  getCatFact()
-  .then((catFact) => {
+  getRandomFact().then((catFact) => {
     paragraph.innerText = catFact;
     notification.classList.add("show");
-    console.log("Showing cat fact:", catFact);
-  })
-  .then(
-        // Hide catfact after set number of ms
-        setTimeout(() => {
-          notification.classList.remove("show");
-        }, 5000)
-  )
-  ;
+    console.log("Showing random fact:", catFact);
+
+    // Hide catfact after certain time
+    setTimeout(() => {
+      notification.classList.remove("show");
+    }, 5000);
+  });
 }
 
 // Checking if it is raining and if it is, fetch a catfact and show it
 function checkRaining() {
-  let city = "Cincinnati";
+  let city = "Columbus";
   const apiKey = "be79937bd89750df6fe78cbc16cdd92d";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -49,8 +43,8 @@ function checkRaining() {
       // Process and display the weather data
       console.log(data); // For debugging
       const weatherCode = data.weather[0].id;
-      if ((weatherCode >= 200) && (weatherCode < 600)) {
-        showCatFact();
+      if (weatherCode >= 200 && weatherCode < 600) {
+        showRandomFact();
         console.log("Showing catfact!");
       }
       console.log(weatherCode);
